0ccd9858be46786726753f1ae5288d5cc58e9c02
lebeili <140650287+lebeili@users.noreply.github.com>
2024-12-20 22:48:42 +0200

    adapter added

--- RadioDevice/Display.h
+++ RadioDevice/Display.h
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
 class Display {
-public:
-
-	Display(); // Constructor
-  void init();
-	void drawGUI();
-  void updateHeader(String text);
-  void updateMainInfo(float frequency, String channel);
-  void updateStatus(int volume);
+	public:
+	  Display(); // Constructor
+	  void init();
+	  void drawGUI();
+	  void updateHeader(String text);
+	  void updateMainInfo(float frequency, String channel);
+	  void updateStatus(int volume);
 
 };
 
 
+class ST77XXAdapter: public Display {
+	public:
+	  ST77XXAdapter(): Display(); // Constructor
+	  void init();
+	  void drawGUI();
+	  void updateHeader(String text);
+	  void updateMainInfo(float frequency, String channel);
+	  void updateStatus(int volume);
+
+};
